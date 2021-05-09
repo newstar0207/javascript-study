7949770a466c39befdab3f3b01ec765ad91227c3
newstar0207 <newstar0207@naver.com>
2021-05-09 23:03:43 +0900

    모던 javascriptTest

--- javascripttest1.js
+++ javascripttest1.js
@@ -265,3 +265,101 @@ function Calculator() {
 
 // console.log( names ); // John, Pete, Mary
 
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = users.map(user /* item */ => ({
+//     fullName : `${user.name} ${user.surname}`,
+//     id : user.id,
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// console.log(( usersMapped[0].id )); // 1
+// console.log(( usersMapped[0].fullName )); // John Smith
+
+
+// function sortByAge(users){
+//     users.sort((a,b) => a.age - b.age);
+// }
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// console.log((arr[0].name)); // John
+// console.log((arr[1].name)); // Mary
+// console.log((arr[2].name)); // Pete
+
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * ( i + 1));
+        let t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+    return array;
+}
+
+// console.log(shuffle([3, 5 , 7]));
+
+function getAverageAge(users){
+    // let sum = 0;
+    // for(let value of users){
+    //     sum += value.age;
+    // }
+    // return +(sum / users.length);
+    return users.reduce((sum, value) => sum + value.age, 0) / users.length;
+}
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+// let arr = [ john, pete, mary ];
+// console.log(getAverageAge(arr));
+
+function unique(arr){
+    let result = [];
+    for (let value of arr){
+        if(!result.includes(value)){
+            result.push(value);
+        }
+    }
+    return result;
+}
+
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// console.log(( unique(strings) )); 
+
+function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
+
+// let users = [
+//     {id: 'john', name: "John Smith", age: 20},
+//     {id: 'ann', name: "Ann Smith", age: 24},
+//     {id: 'pete', name: "Pete Peterson", age: 31},
+//   ];
+  
+//   let usersById = groupById(users);
+//   console.log(usersById);
